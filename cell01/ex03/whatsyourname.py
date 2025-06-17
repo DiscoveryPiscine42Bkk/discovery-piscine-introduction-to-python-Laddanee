4c4323832d34ba309a16369c4af2afa97c486fba
Laddanee <khamsawongladdanee@gmail.com>
2025-06-17 04:20:33 +0000

    cell01 ex03

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,14 @@
+first_name = "Laddanee"
+last_name = "Khamsawong"
+
+s = f"Hey, what's your first name? : {first_name}"
+
+print(s)
+
+o = f"And your last name? : {last_name}"
+
+print(o)
+
+y = f"Well, pleased to meet you, {first_name} {last_name}."
+
+print(y)
