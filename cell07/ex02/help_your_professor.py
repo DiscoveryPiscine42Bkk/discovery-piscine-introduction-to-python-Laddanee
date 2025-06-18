3e2fffaec51ed4792c33c3718154620f796f57a6
Laddanee <khamsawongladdanee@gmail.com>
2025-06-18 19:52:07 +0000

    cell07 ex02

---
+++ cell07/ex02/help_your_professor.py
@@ -0,0 +1,18 @@
+def average(grades):
+    return sum(grades. values()) / len(grades)
+
+class_B = {
+    "p": 1,
+    "r": 5,
+    "a": 8,
+    "w": 9
+}
+
+class_C = {
+    "p": 20,
+    "i": 15,
+    "n": 10,
+    "x": 12
+}
+print(f"Average for class B: {average(class_B)}.")
+print(f"Average for class C: {average(class_C)}.")
